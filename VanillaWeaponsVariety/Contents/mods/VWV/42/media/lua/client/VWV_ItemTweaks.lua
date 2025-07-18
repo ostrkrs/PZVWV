0e9b47ef09238955a8f82bf028b01504c951c969
Osetrokarasek <78341303+ostrkrs@users.noreply.github.com>
2025-07-19 01:33:26 +0500

    Add files via upload

---
+++ VanillaWeaponsVariety/Contents/mods/VWV/42/media/lua/client/VWV_ItemTweaks.lua
@@ -0,0 +1,22 @@
+--JS-2000
+TweakItem("Base.Shotgun","MaxAmmo", "4");
+--Sawn-off JS-2000
+TweakItem("Base.ShotgunSawnoff","MaxAmmo", "4");
+--Sawn-off double-barrel shotgun
+TweakItem("Base.DoubleBarrelShotgunSawnoff","ModelWeaponPart,", "RecoilPad RecoilPad recoilpad recoilpad");
+TweakItem("Base.DoubleBarrelShotgunSawnoff","RecoilDelay", "20")
+TweakItem("Base.DoubleBarrelShotgunSawnoff","ReloadTime ", "30")
+--Double-barrel shotgun
+TweakItem("Base.DoubleBarrelShotgun","RecoilDelay", "20")
+TweakItem("Base.DoubleBarrelShotgun","ReloadTime ", "35")
+
+--2x scope
+TweakItem("Base.x2Scope","MountOn", "HuntingRifle; VarmintRifle; AssaultRifle; AssaultRifle2; Revolver_Long; Shotgun; ShotgunSawnoff; ShotgunExtended; ShotgunSemiExtended; ShotgunSemi; ShotgunSemiSawnoff")
+--Recoil pad
+TweakItem("Base.RecoilPad","MountOn", "HuntingRifle; VarmintRifle; Shotgun; ShotgunSawnoff; DoubleBarrelShotgun; ShotgunExtended; ShotgunSemiExtended; ShotgunSemi; ShotgunSemiSawnoff; DoubleBarrelShotgunSawnoff")
+--Choke full
+TweakItem("Base.ChokeTubeFull","MountOn", "Shotgun; ShotgunExtended; ShotgunSemiExtended; ShotgunSemi")
+--Choke improved
+TweakItem("Base.ChokeTubeImproved","MountOn", "Shotgun; ShotgunExtended; ShotgunSemiExtended; ShotgunSemi")
+--Ammo straps
+TweakItem("Base.AmmoStraps","MountOn", "HuntingRifle; VarmintRifle; Shotgun; ShotgunSawnoff; DoubleBarrelShotgun; ShotgunExtended; ShotgunSemiExtended; ShotgunSemi; ShotgunSemiSawnoff; DoubleBarrelShotgunSawnoff")
---
+++ VanillaWeaponsVariety/Contents/mods/VWV/42/media/lua/client/VWV_ModelTweaks.lua
@@ -0,0 +1,3 @@
+require 'ModelTweaker_Core'
+
+TweakModelAttachment("Base.DoubleBarrelShotgunSawnoff", "attachment recoilpad", "offset = 0.0000 -0.1540 -0.0410, rotate = 10.0000 0.0000 0.0000, scale = 1.13, bone = RecoilPad");
